0824bd954ee0f8303b3a712460bfc300377aae84
Russell Kennington <arussellk@gmail.com>
2016-07-03 21:46:08 -0600

    Add servo control to accelerometer reading

--- adxl345.js
+++ adxl345.js
@@ -3,23 +3,24 @@ var board = new five.Board();
 
 
 board.on('ready', function () {
+    var servo = new five.Servo(10);
     var accelerometer = new five.Accelerometer({
         controller: 'ADXL345'
     });
 
     accelerometer.on('change', function () {
+
         var info = `
-        accelerometer
-            x: ${this.x}
-            y: ${this.y}
-            z: ${this.z}
-            pitch: ${this.pitch}
-            roll: ${this.roll}
-            acceleration: ${this.acceleration}
-            inclination: ${this.inclination}
-            orientation: ${this.orientation}
-        --------------------------------------
-        `;
+accelerometer
+    x: ${this.x}
+    y: ${this.y}
+    z: ${this.z}
+    pitch: ${this.pitch}
+    roll: ${this.roll}
+`;
         console.log(info);
+
+        var normalized = -1 * 2 * (this.pitch) + 90;
+        servo.to(normalized);
     });
 });
